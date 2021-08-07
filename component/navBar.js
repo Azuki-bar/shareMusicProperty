44f08d78c48e3de5288e0b1bb43f78774ec11075
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2021-08-08 03:07:08 +0900

    update Navbar

--- component/navBar.js
+++ component/navBar.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import React, {useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTwitter} from "@fortawesome/free-brands-svg-icons";
 
 export const NavBar = (props) => {
   const [isBurgerOpen, setIsBurgerOpen] = useState(false)
@@ -12,22 +14,10 @@ export const NavBar = (props) => {
   return (
     <div className="NavBar">
       <nav className="navbar" role="navigation" aria-label="main navigation">
-        {/*<div className="navbar-brand">*/}
-        {/*  <div className="navbar-item">*/}
-        {/*    <a href={"https://music.azukibar.dev"}>music.azukibar.dev</a>*/}
-
-        {/*    <i className="fab fa-twitter"/>*/}
-        {/*  </div>*/}
-        {/*  <div className="navbar-menu">*/}
-        {/*    <div className="navbar-item">*/}
-        {/*      <a href={"https://github.azukibar.dev"}>github.azukibar.dev</a>*/}
-        {/*    </div>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
         <div className="navbar-brand">
-          <div id="SiteName" className="navbar-item">
-            <a href="https://music.azukibar.dev">music.azukibar.dev</a>
-          </div>
+          <a id="SiteName" className="navbar-item" href="https://music.azukibar.dev">
+            music.azukibar.dev
+          </a>
 
           <a role="button" className={"navbar-burger" + (isBurgerOpen ? " is-active" : "")}
              aria-label="menu" aria-expanded="false"
@@ -40,51 +30,15 @@ export const NavBar = (props) => {
 
         <div id="navbarBasicExample" className={"navbar-menu" + (isBurgerOpen ? " is-active" : "")}>
           <div className="navbar-start">
-            <div className="navbar-item">
-              <Link href={"/"}>
-                Home
-              </Link>
-            < /div>
-
-            <a className="navbar-item">
-              GitHub
+            <a className="navbar-item" href="https://azukibar.dev">
+              Blog
             </a>
 
-            {/*<div className="navbar-item has-dropdown is-hoverable">*/}
-            {/*  <a className="navbar-link">*/}
-            {/*    More*/}
-            {/*  </a>*/}
-
-            {/*<div className="navbar-dropdown">*/}
-            {/*  <a className="navbar-item">*/}
-            {/*    About*/}
-            {/*  </a>*/}
-            {/*  <a className="navbar-item">*/}
-            {/*    Jobs*/}
-            {/*  </a>*/}
-            {/*  <a className="navbar-item">*/}
-            {/*    Contact*/}
-            {/*  </a>*/}
-            {/*  <hr className="navbar-divider"/>*/}
-            {/*  <a className="navbar-item">*/}
-            {/*    Report an issue*/}
-            {/*  </a>*/}
-            {/*</div>*/}
-            {/*</div>*/}
+            <a className="navbar-item" href="https://twitter.com/azukibar_D">
+              <FontAwesomeIcon icon={faTwitter}/>
+              Twitter
+            </a>
           </div>
-
-          {/*<div className="navbar-end">*/}
-          {/*  <div className="navbar-item">*/}
-          {/*    <div className="buttons">*/}
-          {/*      <a className="button is-primary">*/}
-          {/*        <strong>Sign up</strong>*/}
-          {/*      </a>*/}
-          {/*      <a className="button is-light">*/}
-          {/*        Log in*/}
-          {/*      </a>*/}
-          {/*    </div>*/}
-          {/*  </div>*/}
-          {/*</div>*/}
         </div>
       </nav>
     </div>
